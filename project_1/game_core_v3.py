7f10a62b5e12639d1972b2229a7b698736b9207e
dyuxuh <dyuxuh@gmail.com>
2024-06-11 19:38:19 +0300

    corrections_1

--- project_1/game_core_v3.py
+++ project_1/game_core_v3.py
@@ -1,5 +1,4 @@
-import numpy as np
-def game_core_v3(number: int = 1) -> int:
+def game_core_v3(value: int = 1) -> int:
     """
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -8,17 +7,28 @@ def game_core_v3(number: int = 1) -> int:
         int: Число попыток
     """
     
-    # Создаём переменные
+    #список чисел в котором будем искать искомое
+    a = list(range(1,101))
+    #переменные обозначающие ,левый индекс масссива и правый,а также число попыток
+    left = 0
+    right = len(a) - 1
     count = 0
-    predict = 11
-    predict_number = 1
-    
-    #запускаем цикл угадывания
-    while number != predict_number:
-        count+=1
-        if number > predict:
-            predict+= 10 
-        predict_number =  np.random.randint(predict-10,predict)                    
-    
+    if type(value) != int:
+        raise ValueError('Неверный ввод!')
+    #запускаем цикл, в котором сравниваем искомое число с a[center], если value > a[center] то
+    #ищем число в правой части массива,в противном случае в левой,после чего повторяем алгоритм
+    while left <= right:
+        count += 1
+        center = (left + right) // 2
+        if value == a[center]:
+            break
+        if value > a[center]:
+            left = center + 1
+        else:
+            right = center - 1
+    else:
+        raise ValueError('значение отсутствует')
+        
+    # Ваш код заканчивается здесь
 
     return count
